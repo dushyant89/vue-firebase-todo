cc99911aa75334d16c19ff1beb237b4f557e08a8
dushyant <dushyant.sabharwal@adyen.com>
2018-11-23 14:31:01 +0100

    Fix: importing only the stuff which we need

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 Vue.use(Vuex);
 
